f5948709dce9e160a0d1ffbbfea1dd1c9c0e6c99
Nick Reid <nickreid@nickreid.com>
2016-02-11 12:38:49 -0800

    made http requests work again

--- src/auth/auth.interceptor.js
+++ src/auth/auth.interceptor.js
@@ -3,18 +3,21 @@
 
     angular
         .module('motech-auth')
-        .factory('authHttpInterceptor', ['$rootScope', authHttpInterceptor])
+        .factory('authHttpInterceptor', authHttpInterceptor)
         .config(['$httpProvider', authConfigHttp]);
 
-    function authHttpInterceptor ($rootScope) {
+    authHttpInterceptor.$inject = ['$q', '$rootScope'];
+    function authHttpInterceptor ($q, $rootScope) {
         return {
             'request': function (config) {
                 // if no login
                 // call login event
+                return config;
             },
-            'responseError': function (config) {
+            'responseError': function (rejection) {
                 // if Error && no Login
                 // call login event
+                return $q.reject(rejection);
             }
         }
     }
