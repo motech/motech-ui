42f7c96def9df97c1b19a546aaa1019d1bf1dafb
Nick Reid <nickreid@nickreid.com>
2016-02-12 13:49:23 -0800

    added jshint into build process

--- gulpfile.js/tasks/build.js
+++ gulpfile.js/tasks/build.js
@@ -3,5 +3,13 @@ var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
 gulp.task('build', function () {
-    gulpSequence('assets', 'fonts', 'images', 'index', 'partials', 'sass', 'js', 'uglify')();
+	gulpSequence('build:app', 'build:styleguide')();
+});
+
+gulp.task('build:app', function () {
+    gulpSequence('assets', 'fonts', 'images', 'index', 'partials', 'sass', 'js:hint', 'js:build', 'js:uglify')();
+});
+
+gulp.task('build:styleguide', function() {
+	gulpSequence('styleguide:generate', 'styleguide:applystyles')();
 });
--- gulpfile.js/tasks/javascript.js
+++ gulpfile.js/tasks/javascript.js
@@ -1,36 +1,55 @@
 var config = require('../config');
 var gulp = require('gulp');
+var argv = require('yargs').argv;
 var lib = require('bower-files')();
+var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
+var gulpSequence = require('gulp-sequence');
 var path = require('path');
 
+var sourcePaths = [
+        '**/*.module.js', // modules initialized here
+        '**/*.config.js',
+        '**/*.routes.js',
+        '**/*.js',
+        '*.js' // app.js
+    ];
+
 var paths = {
+    src: sourcePaths.map(function(item){
+        return path.join(config.root.src, item);
+    }),
     dest: path.join(config.assets.dest, 'js')
 }
 
-// Static: Compress JS files into motech.js
 gulp.task('js', function () {
-    var files = lib.ext('js').files; // libraries from bower
-    
-    // load module configuration files first...
-    files.push(path.join(config.assets.src, '**/*.module.js'));
-    files.push(path.join(config.assets.src, '**/*.config.js'));
-    files.push(path.join(config.assets.src, '**/*.routes.js'));
-    files.push(path.join(config.assets.src, '**/*.js'));
-    files.push(path.join(config.assets.src, '*.js')); // app.js
-    
+    gulpSequence('js:hint', 'js:build')();
+});
 
-    return gulp.src(files)
+// Static: Compress JS files into motech.js
+gulp.task('js:build', function () {
+    var files = lib.ext('js').files; // libraries from bower
+    return gulp.src(files.concat(paths.src))
         .pipe(sourcemaps.init())
           .pipe(concat('motech.js'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(paths.dest))
 });
 
-gulp.task('uglify', function (){
+gulp.task('js:hint', function(){
+    var stream = gulp.src(paths.src)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+
+    if(argv.production){
+        stream.pipe(jshint.reporter('fail'));
+    }
+});
+
+gulp.task('js:uglify', function (){
     return gulp.src(path.join(paths.dest, 'motech.js'))
         .pipe(uglify())
         .pipe(rename('motech.min.js'))
