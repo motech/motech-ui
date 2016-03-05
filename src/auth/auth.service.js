92e80582484d0cfbb6aa6bd62542844ced998dac
Nick Reid <nickreid@nickreid.com>
2016-03-05 14:46:03 +0545

    made MOTECH_SERVER_URL a constant

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -4,9 +4,9 @@
 	angular.module('motech-auth')
 		.service('AuthService', AuthService);
 
-	AuthService.$inject = ['$q', '$http'];
-	function AuthService ($q, $http) {
-		var loginUrl = motechServerURL + 'module/server/motech/j_spring_security_check';
+	AuthService.$inject = ['$q', '$http', 'MOTECH_SERVER_URL'];
+	function AuthService ($q, $http, MOTECH_SERVER_URL) {
+		var loginUrl = MOTECH_SERVER_URL + 'module/server/motech/j_spring_security_check';
 
 		this.login = login;
 		this.check = checkAuth;
@@ -22,6 +22,7 @@
 					'X-Requested-With': 'XMLHttpRequest',
 					'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
 				},
+				paramSerializer: '$httpParamSerializerJQLike',
 				withCredentials: true
 			})
 			.then(function () {
---
+++ src/common/common.motech-server.config.js
@@ -0,0 +1,6 @@
+(function () {
+    'use strict';
+
+    angular.module('motech-common')
+    	.constant('MOTECH_SERVER_URL', MOTECH_SERVER_URL);
+})();
