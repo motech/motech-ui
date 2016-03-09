a50feaa5282aad0a102ded841b610992206b5936
Nick Reid <nickreid@nickreid.com>
2016-03-09 15:33:49 +0545

    MOTECH-2125 Removed Ruby dependency
    Added node-sass and node-bourbon, so that entire build process just uses
    nodejs.

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -1,6 +1,7 @@
 var config = require('../config');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var bourbon = require('node-bourbon');
 var lib = require('bower-files')();
 var concat = require('gulp-concat');
 var bless = require('gulp-bless');
@@ -22,7 +23,9 @@ gulp.task('css', function () {
 
 gulp.task('sass', function () {
     gulp.src(path.join(config.root.src,'sass/motech.scss'))
-        .pipe(sass())
+        .pipe(sass({
+            includePaths: bourbon.includePaths
+        }))
         .pipe(concat('motech.css'))
         .pipe(bless())
         .pipe(gulp.dest(paths.dest));
