5cf7415b0cc85692df38ddde98739a4b6d1cac20
Nick Reid <nickreid@nickreid.com>
2016-06-09 16:48:01 -0700

    Replaces SC5 with JSDoc
    Replaced KSS styleguide generator with JSDoc generator because most
    meaningful code is in JS, which is what needs most documentation.
    Created stubs for tutorial entries that will need to be added.

--- gulpfile.js/tasks/build.js
+++ gulpfile.js/tasks/build.js
@@ -3,13 +3,13 @@ var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
 gulp.task('build', function () {
-    gulpSequence('build:app')();
+    gulpSequence('build:app', 'build:docs')();
 });
 
 gulp.task('build:app', function () {
     gulpSequence('assets', 'fonts', 'images', 'partials', 'index.html', 'motech.css', 'motech.js:hint', 'motech.js:build')();
 });
 
-gulp.task('build:styleguide', function() {
-    gulpSequence('styleguide:generate', 'styleguide:applystyles')();
+gulp.task('build:docs', function() {
+    gulpSequence('docs')();
 });
