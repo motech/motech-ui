c18521728e904c4c202a5c39394c31215e0cef78
Nick Reid <nickreid@nickreid.com>
2016-06-09 16:48:01 -0700

    Added JSDoc and removed SC5
    Removed SC5 styleguide in favor of JSDoc. The KSS documentation has been left in SCSS files for now.

--- gulpfile.js/tasks/build.js
+++ gulpfile.js/tasks/build.js
@@ -3,13 +3,9 @@ var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
 gulp.task('build', function () {
-    gulpSequence('build:app')();
+    gulpSequence('build:app', 'docs')();
 });
 
 gulp.task('build:app', function () {
     gulpSequence('assets', 'fonts', 'images', 'partials', 'index.html', 'motech.css', 'motech.js:hint', 'motech.js:build')();
-});
-
-gulp.task('build:styleguide', function() {
-    gulpSequence('styleguide:generate', 'styleguide:applystyles')();
 });
---
+++ gulpfile.js/tasks/docs.js
@@ -0,0 +1,14 @@
+var config = require('../config');
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+
+gulp.task('docs', shell.task([
+  'node_modules/jsdoc/jsdoc.js '+
+    '-c node_modules/angular-jsdoc/common/conf.json '+   // config file
+    '-t node_modules/angular-jsdoc/angular-template '+   // template file
+    '-d ' + config.docs.dest +                           // output directory
+    ' ./OVERVIEW.md ' +                            // to include README.md as index contents
+    '-r ' + config.app.src +               // source code directory
+    ' -u ' + config.docs.src                              // tutorials directory
+]));
