f3612c8c1537ba9105821619594454daf4e52034
Natalia Kowalik <nkowalik@soldevelo.com>
2016-07-04 09:15:34 +0200

    MOTECH-2660: Loading process is handled in the controller

--- src/email/logs.contoller.js
+++ src/email/logs.contoller.js
@@ -4,8 +4,8 @@
     angular.module('motech-email')
         .controller('EmailLogsController', emailLogsController);
 
-    emailLogsController.$inject = ['$compile', '$scope', 'EmailLogsService', 'ModalWindow'];
-    function emailLogsController ($compile, $scope, EmailLogsService, ModalWindow) {
+    emailLogsController.$inject = ['$compile', '$scope', 'EmailLogsService', 'ModalWindow', 'LoadingModal'];
+    function emailLogsController ($compile, $scope, EmailLogsService, ModalWindow, LoadingModal) {
         
         EmailLogsService.get().then(updateRows);
 
@@ -16,8 +16,11 @@
         
         $scope.showExportModal = showExportModal;
         $scope.hideExportModal = hideExportModal;
+        
+        LoadingModal.open();
 
         function changePageTo(page){
+            LoadingModal.open();
             EmailLogsService.getPage(page)
             .then(updateRows);
         }
@@ -26,6 +29,7 @@
             $scope.logs = rows;
             $scope.totalItems = EmailLogsService.totalRecords;
             $scope.currentPage = EmailLogsService.currentPage;
+            LoadingModal.close();
         }
 
         var searchUpdateTimeout;
