bf5e5a6486300aa1e39e7fce04de9f68530c2633
Jakub Redlarski <jredlarski@soldevelo.com>
2016-07-21 14:48:51 +0200

    MOTECH-2639: Added input filter directive
    Input filter group directive allows to filter a list with a set of checkboxes. It takes ng-model and a list of checkboxes which are controlled as an input group and wraps an input filter directive which holds a single checkbox behaviour.

--- src/common/input-filter/input-filter-group.directive.js
+++ src/common/input-filter/input-filter-group.directive.js
@@ -1,6 +1,27 @@
 (function(){
     'use strict';
 
+    /**
+     * @memberOf motech-common
+     * @ngdoc directive
+     * @name  InputFilterGroup
+     * @scope true
+     *
+     * @param { } ng-model Takes variable that contains an object.
+     * @param { } checkboxes List of checkboxes that are controlled as an input group.
+     *
+     * @description
+     * Creates an input group which hold a set of checkboxes and sets all of them as checked on page load.
+     *
+     * This directive expects ng-model to be an object.
+     * Checkboxes should be a list of objects containing two fields 'label' and 'value'.
+     * 'label' is a string which is displayed next to a checkbox and 'value' is a string which would be added to the ng-model when one selects a checkbox.
+     *
+     * @example
+     * <input-filter-group checkboxes="checkboxes" ng-model="variable"></input-filter-group>
+     *
+     */
+
     angular.module('motech-common')
         .directive('inputFilterGroup', inputFilterGroupDirective);
 
--- src/common/input-filter/input-filter.directive.js
+++ src/common/input-filter/input-filter.directive.js
@@ -1,6 +1,29 @@
 (function(){
     'use strict';
 
+    /**
+     * @memberOf motech-common
+     * @ngdoc directive
+     * @name  InputFilter
+     * @scope true
+     *
+     * @param { } ng-model Takes variable that contains an object.
+     * @param { String } value Value of a checkbox which is added to the ng-model.
+     * @param { String } label Label which is displayed next to a checkbox.
+     *
+     * @description
+     * Creates a checkbox which is included in an input group.
+     * A value of a checkbox is added to ng-model when one selects a checkbox and it is removed from ng-model when a certain checkbox is deselected.
+     *
+     * This directive expects ng-model to be an object.
+     * 'label' is a string which is displayed next to a checkbox.
+     * 'value' is a string which would be added to the ng-model when one selects a checkbox.
+     *
+     * @example
+     * <input-filter label="label" ng-model="variable" value="value"></input-filter>
+     *
+     */
+
     angular.module('motech-common')
         .directive('inputFilter', inputFilterDirective);
 
@@ -12,7 +35,7 @@
             require: '^^inputFilterGroup',
             scope: {
                  ngModel: "=",
-                 value: "=?",
+                 value: "=",
                  label: "="
             },
             templateUrl: '/common/input-filter/input-filter.html'
