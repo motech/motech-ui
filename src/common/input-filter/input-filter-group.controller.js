37f10d62dcc51c3a9667a1d446e309b9e0906a12
jredlarski <jredlarski@soldevelo.com>
2016-07-25 22:37:45 +0200

    MOTECH-2639: Added input filter directive (#33)
    * MOTEECH-2639: Start of the filter input directive
    
    * MOTECH-2639: Working directive for set of checkboxes, without one wrapping a set
    
    * MOTECH-2639: Got input filter group working as it should
    
    * MOTECH-2639: Added input filter directive
    
    Input filter group directive allows to filter a list with a set of checkboxes. It takes ng-model and a list of checkboxes which are controlled as an input group and wraps an input filter directive which holds a single checkbox behaviour.
    
    * MOTECH-2639: Improved documentation
    
    Minor changes in documentation for input filter group controller.

---
+++ src/common/input-filter/input-filter-group.controller.js
@@ -0,0 +1,44 @@
+(function(){
+	'use strict';
+
+    /**
+     * @memberOf motech-common
+     * @ngdoc controller
+     * @name InputFilterGroupController
+     *
+     * @description
+     * Contains logic used by the Input Filter Group directive.
+     *
+     * This controls adding all values of checkboxes to the ng-model when ng-model is empty.
+     *
+     */
+
+    angular.module('motech-common')
+        .controller('InputFilterGroupController', inputFilterGroupController);
+
+    inputFilterGroupController.$inject = ['$scope'];
+    function inputFilterGroupController($scope) {
+        var ctrl = this;
+        this.selectAll = selectAllCheckboxes;
+
+        /**
+         *
+         * Adds all values of checkboxes to the ng-model when ng-model is empty.
+         *
+         * @memberOf InputFilterGroupController
+         *
+         */
+
+        function selectAllCheckboxes () {
+            if (!$scope.ngModel) {
+                $scope.ngModel = [];
+                angular.forEach($scope.checkboxes, function(checkbox) {
+                    $scope.ngModel.push(checkbox.value);
+                });
+            }
+        }
+
+        this.selectAll();
+    }
+
+})();
