08435bed798a3b1d4675942444bc1447a4b99eaa
Damian Nowak <dnowak@soldevelo.com>
2016-07-29 14:18:13 +0200

    test 6

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,9 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'junit'],
-
+    junitReporter: {
+        outputDir: 'target/'
+    },
 
     // web server port
     port: 9876,
