6af0ec4927ef0d7cf9de038a27dce4e946f7aaa4
Damian Nowak <dnowak@soldevelo.com>
2016-08-01 14:47:56 +0200

    test 13

--- gulpfile.js/tasks/log.js
+++ gulpfile.js/tasks/log.js
@@ -1,12 +1,16 @@
 var config = require('../config');
 var gulp = require('gulp');
 var path = require('path');
-var jshint = require('gulp-jshint');
- 
+    jshint = require('gulp-jshint'),
+    jshintXMLReporter = require('gulp-jshint-xml-file-reporter');
 gulp.task('log', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('gulp-jshint-jenkins-reporter', {
-	filename: 'target' + '/jshint-checkstyle.xml'
-    }));
+    return gulp.src('src/**/*.js')
+        .pipe(jshint({
+            validthis: true
+        }))
+        .pipe(jshint.reporter(jshintXMLReporter))
+        .on('end', jshintXMLReporter.writeFile({
+            format: 'checkstyle',
+            filePath: 'target/jshint.xml'
+        }));
 });
