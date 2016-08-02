4d87ee4ccd4b46826dc23d03c30305a83d277919
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2016-08-02 14:13:56 +0200

    test fix build

--- src/i18n/i18n.service.spec.js
+++ src/i18n/i18n.service.spec.js
@@ -110,7 +110,7 @@ describe('i18n Service', function() {
     rootScope.$apply();
 
     expect(i18nService.getMessage("sample.message")).not.toEqual("sample.message");
-    expect(i18nService.getMessage("sample.message")).toEqual("mMessage");
+    expect(i18nService.getMessage("sample.message")).toEqual("Message");
   });
 
   it('Can interoperate paramerters into a message', function(){
