ca995a3a45285d67f0829e2f28608142e6a46251
DamianNowakSoldevelo <dnowak@soldevelo.com>
2016-08-08 14:01:18 +0200

    MOTECH-2787: Jasmine tests for Email module (#61)
    * MOTECH-2787: Jasmine tests for Email module
    
    * change after review
    
    * after review
    
    * change after review

---
+++ src/email/export.directive.spec.js
@@ -0,0 +1,28 @@
+describe('Email export directive', function() {
+    var element, compile, scope;
+
+    beforeEach(module('motech-common'));
+    beforeEach(inject(function($compile, $rootScope) {
+        compile = $compile;
+        scope = $rootScope.$new();
+        scope.range = 5;
+        scope.month = 10;
+        element = angular.element('<email-export close="hideExportModal()"></email-export>');
+        compile(element)(scope);
+        scope.$digest();
+    }));
+
+    it('should pass close', function() {
+        expect(element.attr('close')).toBeDefined();
+        expect(element.attr('close')).toEqual('hideExportModal()');
+    });
+
+    it('should have tagName with value "EMAIL-EXPORT"', function() {
+        expect(element.prop("tagName")).toEqual("EMAIL-EXPORT");
+    });
+
+    it('should set range and month element', function() {
+        expect(element.scope().range).toEqual(scope.range);
+        expect(element.scope().month).toEqual(scope.month);
+    });
+});
