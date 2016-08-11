52299507879806b114c459f4a4615f420fdff399
Jakub Hopen <jhopen@soldevelo.com>
2016-08-11 12:24:51 +0200

    MOTECH-2789: jasmine tests for Admin server log

--- src/admin/log/log-options.controller.spec.js
+++ src/admin/log/log-options.controller.spec.js
@@ -1,14 +1,16 @@
 describe('Log options controller', function() {
-    var modalFactory, loadingModal, logOptionsFactory, controller;
+    var modalFactory, loadingModal, logOptionsFactory, controller, scope;
 
     beforeEach(module('motech-admin'));
 
-    beforeEach(inject(function($controller, ModalFactory, LoadingModal, LogOptionsFactory) {
+    beforeEach(inject(function($controller, $rootScope,  ModalFactory, LoadingModal, LogOptionsFactory) {
+        scope = $rootScope.$new();
         modalFactory = ModalFactory;
         loadingModal = LoadingModal;
         logOptionsFactory = LogOptionsFactory;
 
         controller = $controller('LogOptionsController', {
+            $scope: scope,
             ModalFactory: modalFactory,
             LoadingModal: loadingModal,
             LogOptionsFactory: logOptionsFactory
