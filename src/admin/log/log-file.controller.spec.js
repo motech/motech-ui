6438b69c226b0392a96defe52fc928f5ba55630c
Jakub Hopen <jhopen@soldevelo.com>
2016-08-17 10:23:14 +0200

    testing

--- src/admin/log/log-file.controller.spec.js
+++ src/admin/log/log-file.controller.spec.js
@@ -1,23 +1,15 @@
 describe('Log file controller', function(){
     var scope, serverService, logFileFactory, controller, ok;
 
-     beforeEach(function () {
-            module('motech-admin');
-        });
+    beforeEach(module('motech-common'));
 
-    beforeEach(function(){
-        ok = 'nic';
-        console.log("not injected");
-    });
+      beforeEach(inject(function($rootScope, $controller){
+        scope = $rootScope.$new();
+        controller = $controller('InputMapController', {$scope: scope});
+      }));
 
-  beforeEach(inject(function($rootScope){
-    console.log("injected");
-    scope = $rootScope.$new();
-  }));
 
     it('should work', function(){
         expect(scope).toBeDefined();
-        expect(ok).toBeDefined();
-
     });
 });
