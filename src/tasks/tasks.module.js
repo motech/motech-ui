e0255d63252563c69818aa0c909d65a29d658ea9
Paweł Albecki <pawel.albecki@gmail.com>
2016-09-02 11:32:11 +0200

    MOTECH-2875: Migrate Tasks settings (#78)
    * MOTECH-2875: Migrate Tasks settings
    
    * MOTECH-2875: fixes after review
    
    add Jasmine tests for Settings Controller and small changes
    
    * MOTECH-2875: tests fixes after review
    
    * MOTECH-2875: fix

---
+++ src/tasks/tasks.module.js
@@ -0,0 +1,22 @@
+(function(){
+    'use strict';
+
+    /**
+     *  @memberOf motech-tasks
+     *  @ngdoc module
+     *  @name  Motech-Tasks
+     *
+     *  @description
+     *
+     *  Collects components used in Tasks Module
+     */
+
+    angular.module('motech-tasks', [
+        'ngResource',
+        'textAngular',
+        'motech-common',
+        'motech-i18n',
+        'motech-server'
+        ]);
+
+})();
